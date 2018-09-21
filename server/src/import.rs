207881ad5cd5aabd3069d8f4e9ac85e382bb439d
Rob Saunders <rob@wearebrandnew.com>
2018-09-21 13:00:20 +0800

    cache supports pulling down pairs from binance

--- server/src/import.rs
+++ server/src/import.rs
@@ -1,6 +1,7 @@
 extern crate trailer;
 extern crate rusqlite;
 
+use trailer::exchanges::ExchangeAPI;
 use trailer::models::*;
 
 pub fn main() {
@@ -12,6 +13,13 @@ pub fn main() {
     let _ = delete_all_from(&conn, "klines");
 
     let client = ::trailer::exchanges::binance::connect("9N5duztMdrYfYg2ErhSDV837s8xfBIqF8D7mxpJTKiujvSwoIDI52UguhhkyRQBg", "OG6avXJGOeDt5Phbp150zeEgwjQZpgkXdrp8z2vwPv5bWlHuNFLrK4uAGidnpAIU");
+
+    let prices = client.prices().expect("fetch prices failed");
+
+    for price in prices {
+        insert_pair(&conn, price.clone(), "binance").expect("price to insert");
+        println!("inserted {:?}", price);
+    }
 
     let pairs = vec!["XEMBTC".to_string(), "ADABTC".to_string()];
     let data = ::trailer::threadpool::chart_data(::std::sync::Arc::new(client.clone()), pairs.clone(), "15m");
@@ -32,6 +40,18 @@ pub fn main() {
 
 fn delete_all_from(conn: &rusqlite::Connection, table: &str) -> Result<i32, rusqlite::Error> {
     Ok(conn.execute(&format!("DELETE FROM {}", table), &[])?)
+}
+
+fn insert_pair(conn: &rusqlite::Connection, price: Price, exchange: &str) -> Result<i32, rusqlite::Error> {
+    let (pair, price) = price;
+    Ok(conn.execute("INSERT INTO pairs (pair, price, exchange)
+      VALUES (?1, ?2, ?3)",
+      &[
+        &pair,
+        &price.to_string(),
+        &exchange.to_string(),
+      ])?
+    )
 }
 
 // returns row id
--- server/src/setup.rs
+++ server/src/setup.rs
@@ -8,18 +8,27 @@ pub fn main() {
         .expect("rusqlite connection could not open on disk");
 
     conn.execute_batch("
+         DROP TABLE IF EXISTS pairs;
          DROP TABLE IF EXISTS klines;
     ").expect("error deleting databases");
 
     conn.execute_batch("
+        CREATE TABLE pairs (
+            id                  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            pair                VARCHAR NOT NULL,
+            exchange            VARCHAR NOT NULL,
+            volume              REAL,
+            price               REAL
+        );
+
         CREATE TABLE klines (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            pair VARCHAR NOT NULL,
-            time_period VARCHAR NOT NULL,
-            exchange VARCHAR NOT NULL,
-            volume REAL,
-            close_price REAL,
-            number_of_trades INTEGER
+            id                  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            pair                VARCHAR NOT NULL,
+            time_period         VARCHAR NOT NULL,
+            exchange            VARCHAR NOT NULL,
+            volume              REAL,
+            close_price         REAL,
+            number_of_trades    INTEGER
         );
     ").expect("error regenerating databases");
 }
