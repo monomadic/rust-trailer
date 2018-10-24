7ebed5bacf5f35bbc509dc959b42290f9a215633
Rob Saunders <rob@wearebrandnew.com>
2018-10-24 17:28:37 +0800

    cli: display order with price

--- cli/src/display/order.rs
+++ cli/src/display/order.rs
@@ -11,7 +11,8 @@ pub fn row(order: Order) -> String {
 pub fn sub_row(presenter: OrderPresenter) -> String {
     format!("{:<9}\t{:20}\t{:20.8}\t{:20.2}\n",
         trade_type(presenter.order.order_type),
-        format!("{:.2} btc (${:.0})", presenter.current_value_in_btc(), presenter.current_value_in_usd()),
+        // format!("{:.2} btc (${:.0})", presenter.current_value_in_btc(), presenter.current_value_in_usd()),
+        ::display::btc_and_usd_price(presenter.current_value_in_btc(), presenter.current_value_in_usd()),
         presenter.order.price,
         presenter.order.qty
     )
