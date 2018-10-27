450196bbfd4a2ff9aa4c9a71f89a735b14ee5b4e
Rob Saunders <rob@wearebrandnew.com>
2018-10-27 23:05:55 +0800

    fixed calculation bug on potential profit

--- src/presenters/position.rs
+++ src/presenters/position.rs
@@ -38,8 +38,8 @@ impl PositionPresenter {
         // price of remaining units at the current price - those units at buy price
         // (self.position.remaining_quantity() * self.current_price) - (self.position.remaining_quantity() * self.position.entry_price())
 
-        self.position.remaining_quantity() * (self.current_price - self.position.entry_price())
-        // self.position.remaining_quantity() * self.current_price
+        // self.position.remaining_quantity() * (self.current_price - self.position.entry_price())
+        ((self.current_price * self.position.remaining_quantity()) - (self.position.entry_price() * self.position.remaining_quantity()))
     }
 
     pub fn unrealised_profit_usd(&self) -> f64 {
@@ -72,30 +72,33 @@ pub fn total_btc_staked(presenters: Vec<PositionPresenter>) -> f64 {
     presenters.into_iter().map(|a| a.current_value_in_btc()).sum()
 }
 
-// ------ tests
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn order_fixture(order_type: TradeType, qty: f64, price: f64) -> Order {
-    Order{ id: "".to_string(), symbol: "".to_string(), order_type: order_type, qty: qty, price: price }
-}
+    fn order_fixture(order_type: TradeType, qty: f64, price: f64) -> Order {
+        Order{ id: "".to_string(), symbol: "".to_string(), order_type: order_type, qty: qty, price: price }
+    }
 
-#[test]
-fn test_position_presenter_state_partial() {
-    let position = Position::new(vec![
-        order_fixture(TradeType::Buy, 2.0, 100.0), // value: 200
-        order_fixture(TradeType::Sell, 1.0, 100.0), // sold: 100 worth, remaining: 100, profit: 0
-    ]);
+    #[test]
+    fn test_position_presenter_state_partial() {
+        let position = Position::new(vec![
+            order_fixture(TradeType::Buy, 2.0, 100.0), // value: 200
+            order_fixture(TradeType::Sell, 1.0, 100.0), // sold: 100 worth, remaining: 100, profit: 0
+        ]);
 
-    let presenter = PositionPresenter {
-        position:           position.first().unwrap().clone(),
-        current_price:      110.0,
-        btc_price_in_usd:   2.0,
-    };
+        let presenter = PositionPresenter {
+            position:           position.first().unwrap().clone(),
+            current_price:      110.0,
+            btc_price_in_usd:   2.0,
+        };
 
-    assert_eq!(presenter.current_value_in_btc(), 110.0);
-    assert_eq!(presenter.current_value_in_usd(), 220.0);
-    assert_eq!(presenter.percent_change(), 10.0);
-    assert_eq!(presenter.total_profit_btc(), 11.0); // current btc value of profit
-    assert_eq!(presenter.total_profit_usd(), 22.0);
-    // assert_eq!(presenter.unrealised_profit_btc(), 20.0);
-    assert_eq!(presenter.realised_profit_btc(), 0.0);
+        assert_eq!(presenter.current_value_in_btc(), 110.0);
+        assert_eq!(presenter.current_value_in_usd(), 220.0);
+        assert_eq!(presenter.percent_change(), 10.0);
+        assert_eq!(presenter.total_profit_btc(), 11.0); // current btc value of profit
+        assert_eq!(presenter.total_profit_usd(), 22.0);
+        // assert_eq!(presenter.unrealised_profit_btc(), 20.0);
+        assert_eq!(presenter.realised_profit_btc(), 0.0);
+    }
 }
