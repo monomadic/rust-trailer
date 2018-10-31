22851fa0ca871be44d2581f0931c0605c96d7090
Rob Saunders <rob@wearebrandnew.com>
2018-10-31 10:37:51 +0800

    web: fixed compile errors

--- server/src/views/position.rs
+++ server/src/views/position.rs
@@ -19,15 +19,15 @@ pub fn row(presenter: PositionPresenter) -> String {
     format!("{:#?}", position)
 }
 
-pub fn profit_loss(presenter: PositionPresenter) -> String {
-    match presenter.position.state() {
-        PositionState::Closed | PositionState::Irreconciled => "".to_string(),
-        _ => 
-            colored_number(
-                presenter.percent_change(),
-                format!("{:.2}% (${:.2}, {:.8} btc)", presenter.percent_change(), presenter.total_profit_usd(), presenter.total_profit_btc())),
-    }
-}
+// pub fn profit_loss(presenter: PositionPresenter) -> String {
+//     match presenter.position.state() {
+//         PositionState::Closed | PositionState::Irreconciled => "".to_string(),
+//         _ => 
+//             colored_number(
+//                 presenter.percent_change(),
+//                 format!("{:.2}% (${:.2}, {:.8} btc)", presenter.percent_change(), presenter.total_profit_usd(), presenter.total_profit_btc())),
+//     }
+// }
 
 pub fn print_price(percent: f64, usd: f64, btc: f64) -> String {
     colored_number(
@@ -36,15 +36,15 @@ pub fn print_price(percent: f64, usd: f64, btc: f64) -> String {
     )
 }
 
-pub fn unrealised_profit(presenter: PositionPresenter) -> String {
-    match presenter.position.state() {
-        PositionState::Closed | PositionState::Irreconciled => "".to_string(),
-        _ => 
-            colored_number(
-                presenter.percent_change(),
-                format!("{:.2}% (${:.2}, {:.4} btc)", presenter.percent_change(), presenter.total_profit_usd(), presenter.total_profit_btc())),
-    }
-}
+// pub fn unrealised_profit(presenter: PositionPresenter) -> String {
+//     match presenter.position.state() {
+//         PositionState::Closed | PositionState::Irreconciled => "".to_string(),
+//         _ => 
+//             colored_number(
+//                 presenter.percent_change(),
+//                 format!("{:.2}% (${:.2}, {:.4} btc)", presenter.percent_change(), presenter.total_profit_usd(), presenter.total_profit_btc())),
+//     }
+// }
 
 pub fn position_state(state: PositionState) -> String {
     match state {
@@ -52,6 +52,14 @@ pub fn position_state(state: PositionState) -> String {
         PositionState::Partial      => "PART".to_string(),
         PositionState::Closed       => "CLOSED".to_string(),
         PositionState::Irreconciled => "IRREC".to_string(),
+        PositionState::Invalid      => "INVALID".to_string(),
+    }
+}
+
+pub fn position_size_compact(presenter: PositionPresenter) -> String {
+    match presenter.position.state()  {
+        PositionState::Closed | PositionState::Irreconciled => format!("{:.2} btc, ${:.2}", presenter.position.buy_cost(), presenter.current_value_in_usd()),
+        _ => format!("{:.2} btc, ${:.2}", presenter.current_value_in_btc(), presenter.current_value_in_usd()),
     }
 }
 
